5395d97967f62c5a8277010a12e9ac493cfd4373
Jeff Shindelus <shindelu@adobe.com>
2020-04-05 18:15:51 -0700

    Initial Commit

---
+++ Pong/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pong
+//
+//  Created by shindelu on 4/5/20.
+//  Copyright © 2020 shindelu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
